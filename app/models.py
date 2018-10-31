17e9bac08b1df4cb15bb8ec3d0b9ee535901643b
DongHoony <31026350+DongHoony@users.noreply.github.com>
2018-10-31 21:41:54 +0900

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class MemberBusAlert(models.model):
+    user_key = models.CharField(max_length = 20)
+    bus_num = models.CharField(max_length = 4)
+    goschool_stn = models.CharField(max_length = 5)
+    gohome_stn = models.CharField(max_length = 5)
