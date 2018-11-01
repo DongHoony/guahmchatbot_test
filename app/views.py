e0784db4991b9b4544b55575d0f7f7affe83b01e
DongHoony <31026350+DongHoony@users.noreply.github.com>
2018-11-01 09:28:38 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -136,16 +136,16 @@ def foodie(n):
         for i in range(1, 6):
             td1[i] = str(td1[i])
             td2[i] = str(td2[i])
-            tempdish1 = td1[i].replace('<td class="textC">', '').replace('<br/>', '\n', -1).replace('</td>', '').replace('\n','\n-',-1)
-            dish1 = '- '
+            tempdish1 = td1[i].replace('<td class="textC">', '').replace('<br/>', '\n', -1).replace('</td>', '').replace('\n','\n- ',-1)
+            dish1 = '========\n- '
             for _ in tempdish1:
                 if _ in '1234567890.':
                     continue
                 else:
                     dish1 += _
 
-            tempdish2 = td2[i].replace('<td class="textC">', '').replace('<br/>', '\n', -1).replace('</td>', '').replace('\n','\n-',-1)
-            dish2 = '- '
+            tempdish2 = td2[i].replace('<td class="textC">', '').replace('<br/>', '\n', -1).replace('</td>', '').replace('\n','\n- ',-1)
+            dish2 = '========\n- '
             for _ in tempdish2:
                 if _ in '1234567890.':
                     continue
