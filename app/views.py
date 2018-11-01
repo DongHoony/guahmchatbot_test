dadd07efb9e528db34a46bd70272c412600daf80
DongHoony <31026350+DongHoony@users.noreply.github.com>
2018-11-01 11:53:13 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -25,17 +25,17 @@ try:
 except sqlite3.OperationalError:
     print("Table already exists, Skip making table...")
 
-# BusStops, to School
-schoolBusStop13 = ['벽산아파트', '약수맨션', '노량진역', '대방역2번출구앞']
-schoolBusStop5513 = ['관악구청', '서울대입구', '봉천사거리, 봉천중앙시장', '봉현초등학교', '벽산블루밍벽산아파트303동앞']
+# BusStops, to School // Moved to dict
+# schoolBusStop13 = ['벽산아파트', '약수맨션', '노량진역', '대방역2번출구앞']
+# schoolBusStop5513 = ['관악구청', '서울대입구', '봉천사거리, 봉천중앙시장', '봉현초등학교', '벽산블루밍벽산아파트303동앞']
 
 # BusStops, to Home
 homeBusStop13 = ['관악드림타운북문 방면 (동작13)', '벽산아파트 방면 (동작13)']
 homeBusStop5513 = ['관악드림타운북문 방면 (5513)', '벽산아파트 방면 (5513)']
 
-# BusStop values, to School
-numBusStop13 = ['21910', '20891', '20867', '20834']
-numBusStop5513 = ['21130', '21252', '21131', '21236', '21247']
+# BusStop values, to School // Moved to dict
+# numBusStop13 = ['21910', '20891', '20867', '20834']
+# numBusStop5513 = ['21130', '21252', '21131', '21236', '21247']
 
 # Setting lines
 setting13 = ['벽산아파트 (설정)', '약수맨션 (설정)', '노량진역 (설정)', '대방역2번출구앞 (설정)']
@@ -256,7 +256,7 @@ def message(request):
         )
 
     elif clickedButton in setting13:
-        bus_stn_setting_list.append(numBusStop13[setting13.index(clickedButton)])
+        bus_stn_setting_list.append(bus_stop_dict_13.get(clickedButton.replace(' (설정)', '')))
         return JsonResponse(
             {
                 'message': {
@@ -300,7 +300,7 @@ def message(request):
         )
 
     elif clickedButton in setting5513:
-        bus_stn_setting_list.append(numBusStop5513[setting5513.index(clickedButton)])
+        bus_stn_setting_list.append(bus_stop_dict_5513.get(clickedButton.replace(' (설정)', '')))
         return JsonResponse(
             {
                 'message': {
@@ -365,9 +365,8 @@ def message(request):
                 }
             )
         if school[1] == 13:
-            n = [0, 1, 1, 2][numBusStop13.index(school[2])]
-            busList = bus(n, school[2], 13)
-            bus01, bus02, tayo1, tayo2 = map(str, busList)
+            n = [0, 1, 1, 2][list(bus_stop_dict_13.keys()).index(school[2])]
+            bus01, bus02, tayo1, tayo2 = map(str, bus(n, school[2], 13))
             return JsonResponse(
                 {
                     'message': {
@@ -390,7 +389,7 @@ def message(request):
                 }
             )
         else:
-            n = [5, 1, 7, 2, 0][numBusStop5513.index(school[2])]
+            n = [5, 1, 7, 2, 0][list(bus_stop_dict_5513.keys()).index(school[2])]
             bus(n, school[2], 5513)
             busList = bus(n, school[2], 5513)
             bus01, bus02 = map(str, busList)
@@ -510,7 +509,7 @@ def message(request):
             }
         )
 
-    elif clickedButton in schoolBusStop13:
+    elif clickedButton in bus_stop_dict_13.values():
         busStop = bus_stop_dict_13.get(clickedButton)
         n = [0, 1, 1, 2][list(bus_stop_dict_13.values()).index(clickedButton)]
         bus01, bus02, tayo1, tayo2 = map(str, bus(n, busStop, 13))
@@ -530,7 +529,7 @@ def message(request):
             }
         )
 
-    elif clickedButton in schoolBusStop5513:
+    elif clickedButton in bus_stop_dict_5513.values():
         busStop = bus_stop_dict_5513.get(clickedButton)
         n = [5, 1, 7, 2, 0][list(bus_stop_dict_5513.values()).index(clickedButton)]
         bus01, bus02 = map(str, bus(n, busStop, 5513))
@@ -579,8 +578,7 @@ def message(request):
 
     elif clickedButton in homeBusStop13:
         busStop = ['21244', '21243'][homeBusStop13.index(clickedButton)]
-        busList = bus(1, busStop, 13)
-        bus01, bus02, tayo1, tayo2 = map(str, busList)
+        bus01, bus02, tayo1, tayo2 = map(str, bus(1, busStop, 13))
         return JsonResponse(
             {
                 'message': {
@@ -597,8 +595,7 @@ def message(request):
 
     elif clickedButton in homeBusStop5513:
         busStop = ['21244', '21243'][homeBusStop5513.index(clickedButton)]
-        busList = bus(2, busStop, 5513)
-        bus01, bus02 = map(str, busList)
+        bus01, bus02 = map(str, bus(2, busStop, 5513))
         return JsonResponse(
             {
                 'message': {
